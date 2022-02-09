0862c3bca6e9c9a01131a5cae229696f7a71c539
gups <91670399+gsinghg19@users.noreply.github.com>
2022-02-09 15:30:11 +0000

    altered controller code for viewing article comments via article_id

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -56,3 +56,14 @@ exports.getAllArticles = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getCommentsByArticleId = async (req, res, next) => {
+  try {
+    const { article_id } = req.params;
+    //console.log("line6<<<<>>>>", req);
+    const commentsByArticleId = await fetchCommentsByArticleId(article_id);
+    res.status(200).send({ commentsByArticleId });
+  } catch (err) {
+    next(err);
+  }
+};
